e641b5a8a3b85f428a0ae8cfe5fa84218333d39e
Matus Smolka <matuskosmolka@gmail.com>
2024-04-08 13:01:13 +0200

    funguje

--- searching.py
+++ searching.py
@@ -14,14 +14,28 @@ def read_data(file_name, field):
 
     file_path = os.path.join(cwd_path, file_name)
     with open(file_path) as data_file:
-        allowed_keys = json.load(f)["allowed_keys"]
-
-    return data.get(field,None)
-
+        data=json.load(data_file)
+    for key in data.keys():
+        if field == key:
+            sequential_data=data[field]
+            return sequential_data
+    return None
+def linear_search(seq,looking_for):
+    splitedseq=[*seq]
+    looking_for=[*looking_for]
+    pozition= []
+    count=0
+    for idx in range(0,len(splitedseq)) :
+        if splitedseq[idx:(idx+len(looking_for))]==looking_for :
+            pozition.append(idx)
+            count=count+1
+    return pozition,count
 def main():
-    sequential_data= read_data("sequential.json","unordered_numbers")
+    sequential_data= read_data("sequential.json","dna_sequence")
     print(sequential_data)
-
+    searching_for="GA"
+    search_for = linear_search(sequential_data,searching_for)
+    print(search_for)
 
 if __name__ == '__main__':
     main()
